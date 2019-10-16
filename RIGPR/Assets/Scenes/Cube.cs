03faded14bd8babe7baab1d9cb7bda2038e2bb22
Cristi <up825241@myport.ac.uk>
2019-10-16 15:05:40 +0100

    Added the unity proj

---
+++ RIGPR/Assets/Scenes/Cube.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    float speed = 5f;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void MoveUP()
+    {
+        transform.position += new Vector3(0, 1, 0) * speed * Time.deltaTime;
+    }
+    public void MoveDown()
+    {
+        transform.position += new Vector3(0, -1, 0) * speed * Time.deltaTime;
+    }
+    public void MoveRight()
+    {
+        transform.position += new Vector3(1, 0, 0) * speed * Time.deltaTime;
+    }
+    public void MoveLeft()
+    {
+        transform.position += new Vector3(-1, 0, 0) * speed * Time.deltaTime;
+    }
+}
