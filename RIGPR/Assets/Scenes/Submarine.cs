00e3b87fe33133020acac28b38a9b672e0035eca
Cristi <up825241@myport.ac.uk>
2019-10-18 14:25:58 +0100

    starting the sub script

---
+++ RIGPR/Assets/Scenes/Submarine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Submarine : MonoBehaviour
+{
+
+    float speed = 3.0f, pitch, roll, yaw, mass;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
